1582e3c37be166f0594c36bf3bdb429b3bd1b726
Elif Yıldırım <yildirimelif726@gmail.com>
2021-03-07 13:11:43 +0300

    .

--- Core/Entities/Concrete/UserOperationClaim.cs
+++ Core/Entities/Concrete/UserOperationClaim.cs
@@ -2,6 +2,8 @@
 {
     public class UserOperationClaim : IEntity
     {
+        public int UserId;
+
         public int Id { get; set; }
         public string Name { get; set; }
         public int OperationClaimId { get; set; }
